b657a0b32045c54cd33ed5cef6b5bb14e22c917a
Christian Russo <christian.rss@eduge.ch>
2020-02-03 15:02:09 +0100

    Créeation de l'interface graphique de la page d'accueil et de la fenêtre de création de compte

---
+++ Programme/Filmap/Filmap/frmAccueil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Filmap
+{
+    public partial class frmAccueil : Form
+    {
+        public frmAccueil()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreationCompte_Click(object sender, EventArgs e)
+        {
+            frmCreationCompte signupForm = new frmCreationCompte();
+            signupForm.ShowDialog();
+        }
+
+        private void btnConnection_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInvite_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Programme/Filmap/Filmap/frmCreationCompte.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Filmap
+{
+    partial class frmCreationCompte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitre = new System.Windows.Forms.Label();
+            this.lblPseudo = new System.Windows.Forms.Label();
+            this.lblMotDePasse = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnCreationCompte = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitre
+            // 
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitre.Location = new System.Drawing.Point(26, 21);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(262, 34);
+            this.lblTitre.TabIndex = 0;
+            this.lblTitre.Text = "Créer son compte";
+            // 
+            // lblPseudo
+            // 
+            this.lblPseudo.AutoSize = true;
+            this.lblPseudo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPseudo.Location = new System.Drawing.Point(28, 85);
+            this.lblPseudo.Name = "lblPseudo";
+            this.lblPseudo.Size = new System.Drawing.Size(121, 19);
+            this.lblPseudo.TabIndex = 1;
+            this.lblPseudo.Text = "Votre pseudo :";
+            // 
+            // lblMotDePasse
+            // 
+            this.lblMotDePasse.AutoSize = true;
+            this.lblMotDePasse.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMotDePasse.Location = new System.Drawing.Point(28, 141);
+            this.lblMotDePasse.Name = "lblMotDePasse";
+            this.lblMotDePasse.Size = new System.Drawing.Size(165, 19);
+            this.lblMotDePasse.TabIndex = 2;
+            this.lblMotDePasse.Text = "Votre mot de passe :";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(28, 114);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(103, 19);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Votre email :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(201, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(201, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(201, 142);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '•';
+            this.textBox3.Size = new System.Drawing.Size(189, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // btnCreationCompte
+            // 
+            this.btnCreationCompte.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnCreationCompte.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCreationCompte.FlatAppearance.BorderSize = 0;
+            this.btnCreationCompte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreationCompte.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreationCompte.Location = new System.Drawing.Point(280, 172);
+            this.btnCreationCompte.Name = "btnCreationCompte";
+            this.btnCreationCompte.Size = new System.Drawing.Size(110, 26);
+            this.btnCreationCompte.TabIndex = 8;
+            this.btnCreationCompte.Text = "Créer un compte";
+            this.btnCreationCompte.UseVisualStyleBackColor = false;
+            this.btnCreationCompte.Click += new System.EventHandler(this.btnCreationCompte_Click);
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAnnuler.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAnnuler.FlatAppearance.BorderSize = 0;
+            this.btnAnnuler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnnuler.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnnuler.Location = new System.Drawing.Point(201, 172);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(73, 26);
+            this.btnAnnuler.TabIndex = 9;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = false;
+            this.btnAnnuler.Click += new System.EventHandler(this.btnAnnuler_Click);
+            // 
+            // CreationCompte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(419, 210);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnCreationCompte);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblMotDePasse);
+            this.Controls.Add(this.lblPseudo);
+            this.Controls.Add(this.lblTitre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "CreationCompte";
+            this.Text = "Créer son compte";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitre;
+        private System.Windows.Forms.Label lblPseudo;
+        private System.Windows.Forms.Label lblMotDePasse;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnCreationCompte;
+        private System.Windows.Forms.Button btnAnnuler;
+    }
+}
