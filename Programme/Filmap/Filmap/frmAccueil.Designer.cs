163c891844c8d342105062b8392d9bcfc70ea169
tiago-gmfrr <tiago.gmfrr@eduge.ch>
2020-03-02 13:57:30 +0100

    refactorisation de frmAccueil -> creation classe dbConnection

--- Programme/Filmap/Filmap/frmAccueil.Designer.cs
+++ Programme/Filmap/Filmap/frmAccueil.Designer.cs
@@ -146,6 +146,7 @@
             // 
             // frmAccueil
             // 
+            this.AcceptButton = this.btnConnection;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
